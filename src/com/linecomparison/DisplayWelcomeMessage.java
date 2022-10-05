56e3e620be5d53cdff062a4631c7a9b9b02de929
hhmadan <hhmadan03@gmail.com>
2022-10-06 01:27:39 +0530

    Welcome Message on Master branch

---
+++ src/com/linecomparison/DisplayWelcomeMessage.java
@@ -0,0 +1,8 @@
+// Program to Display Welcome to Line Comparison Computation
+package com.linecomparison;
+
+public class DisplayWelcomeMessage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation");
+    }
+}
